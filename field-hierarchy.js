f3dff7d2e472999916907178dfa3b03e4874046e
Aaron Dennis <aaron.p.dennis@gmail.com>
2015-06-26 12:11:14 -0400

    added files

---
+++ field-hierarchy.js
@@ -0,0 +1,127 @@
+// A hierarchy of all data fields
+fieldHierarchy = [
+  {
+    field: "INFORM RISK",
+    subset: ["VULNERABILITY", "HAZARD & EXPOSURE", "LACK OF COPING CAPACITY"]
+  },
+  {
+    field: "VULNERABILITY",
+    subset: ["Socio-Economic Vulnerability", "Vulnerable Groups"]
+  },
+  {
+    field: "Socio-Economic Vulnerability",
+    subset: ["Development & Deprivation", "Inequality", "Aid Dependency"]
+  },
+  {
+    field: "Development & Deprivation",
+    subset: ["Development & Deprivation"]
+  },
+  {
+    field: "Inequality",
+    subset: ["Inequality"]
+  },
+  {
+    field: "Aid Dependency",
+    subset: ["Aid Dependency"]
+  },
+  {
+    field: "Vulnerable Groups",
+    subset: ["Uprooted people", "Health Conditions", "Children U5", "Recent Shocks", "Food Security", "Other Vulnerable Groups"]
+  },
+  {
+    field: "Uprooted people",
+    subset: ["Uprooted people"]
+  },
+  {
+    field: "Health Conditions",
+    subset: ["Health Conditions"]
+  },
+  {
+    field: "Children U5",
+    subset: ["Children U5"]
+  },
+  {
+    field: "Recent Shocks",
+    subset: ["Recent Shocks"]
+  },
+  {
+    field: "Food Security",
+    subset: ["Food Security"]
+  },
+  {
+    field: "Other Vulnerable Groups",
+    subset: ["Other Vulnerable Groups"]
+  },
+  {
+    field: "LACK OF COPING CAPACITY",
+    subset: ["Institutional", "Infrastructure"]
+  },
+  {
+    field: "Institutional",
+    subset: ["DRR", "Governance"]
+  },
+  {
+    field: "DRR",
+    subset: ["DRR"]
+  },
+  {
+    field: "Governance",
+    subset: ["Governance"]
+  },
+  {
+    field: "Infrastructure",
+    subset: ["Communication", "Physical infrastructure", "Access to health care"]
+  },
+  {
+    field: "Communication",
+    subset: ["Communication"]
+  },
+  {
+    field: "Physical infrastructure",
+    subset: ["Physical infrastructure"]
+  },
+  {
+    field: "Access to health care",
+    subset: ["Access to health care"]
+  },
+  {
+    field: "HAZARD & EXPOSURE",
+    subset: ["Natural", "Human"]
+  },
+  {
+    field: "Natural",
+    subset: ["Earthquake", "Flood", "Tsunami", "Tropical Cyclone", "Drought"]
+  },
+  {
+    field: "Earthquake",
+    subset: ["Earthquake"]
+  },
+  {
+    field: "Flood",
+    subset: ["Flood"]
+  },
+  {
+    field: "Tsunami",
+    subset: ["Tsunami"]
+  },
+  {
+    field: "Tropical Cyclone",
+    subset: ["Tropical Cyclone"]
+  },
+  {
+    field: "Drought",
+    subset: ["Drought"]
+  },
+  {
+    field: "Human",
+    subset: ["Projected Conflict Risk", "Current Highly Violent Conflict Intensity"]
+  },
+  {
+    field: "Projected Conflict Risk",
+    subset: ["Projected Conflict Risk"]
+  },
+  {
+    field: "Current Highly Violent Conflict Intensity",
+    subset: ["Current Highly Violent Conflict Intensity"]
+  }
+];
